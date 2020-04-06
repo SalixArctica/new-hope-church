306d0ca8036e9886804e1f82dc0a8293012825f0
Tankcaster <tankcaster@gmail.com>
2020-04-06 09:06:38 -0500

    remove index page preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,5 @@
 import CMS from "netlify-cms-app"
 
-import IndexPagePreview from "./preview-templates/IndexPagePreview"
 import eventPreview from "./preview-templates/eventPreview"
 
-CMS.registerPreviewTemplate("index", IndexPagePreview)
 CMS.registerPreviewTemplate("events", eventPreview)
